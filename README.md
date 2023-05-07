c15e6b76f427c0f8a2d45b4a31d36e9e40256cd2
0xSchierke <103627996+0xSudarshan@users.noreply.github.com>
2023-05-07 20:04:45 +0530

    initial-commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # protocol-core-contracts
 This repo contains all the core-protocol contracts
+open terminal -->> npx hardhat node
+open another terminal ->> npx hardhat run --network localhost scripts/deploy.ts
+npx hardhat console --network localhost
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks:{
+    hardhat:{
+      forking:{
+        url:"https://eth-mainnet.g.alchemy.com/v2/_9FneSnR-nKwMxo4Hybym-e9VAyy139z"
+      }
+    }
+  }
 };
 
 export default config;
